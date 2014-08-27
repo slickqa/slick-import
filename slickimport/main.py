ce44fd83a4aa6c0b6350c202c81a22eb2f272662
Jason Corbett <jcorbett@accessdata.com>
2014-08-27 16:28:35 -0600

    now connecting to slick, and validating the connection

--- slickimport/main.py
+++ slickimport/main.py
@@ -8,6 +8,10 @@ import os
 import sys
 import argparse
 import pkg_resources
+import slickqa
+import traceback
+
+
 
 def main(args=sys.argv[1:]):
     parser = argparse.ArgumentParser(prog='slick-import',
@@ -16,6 +20,7 @@ def main(args=sys.argv[1:]):
                                                  'http://github.com/slickqa/slick-import',
                                      version=pkg_resources.get_distribution('slickqa-slick-import').version)
     parser.add_argument('--delete', action='store_true', help='Delete files once their content is synced.')
+    parser.add_argument('-u', '--url', required=True, help='Base URL of slick, must be specified.')
     parser.add_argument('path', metavar='DIR', nargs=1, help='Path to the directory structure to import.')
     params = parser.parse_args(args)
 
@@ -28,3 +33,22 @@ def main(args=sys.argv[1:]):
         parser.print_usage(file=sys.stderr)
         print('ERROR: Invalid path "{}", path must be a directory.\n'.format(params.path[0]), file=sys.stderr)
         sys.exit(1)
+
+    slick = None
+    try:
+        slick = slickqa.SlickConnection(params.url)
+        version = slick.version.findOne()
+        print('Connected to slick version "{}" at "{}".'.format(version.versionString, params.url))
+    except slickqa.SlickCommunicationError:
+        parser.print_usage(file=sys.stderr)
+        print('ERROR: Unable to communicate with slick at URL: ', params.url, file=sys.stderr)
+        traceback.print_exc(file=sys.stderr)
+
+
+def import_configurations(slick, path):
+    """
+
+    :param slick:
+    :param path:
+    :return:
+    """
