2b56b3f97af7d4d29e6d44d1f75e8882e918a2b7
Jason Corbett <jcorbett@accessdata.com>
2014-08-27 14:15:58 -0600

    adding to setup, and adding in python files

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ use_setuptools()
 
 from setuptools import setup, find_packages
 
+requirements = []
+with open('requirements.txt', 'r') as reqfile:
+    requirements.extend(reqfile.read().split())
+
 setup(
     name="slickqa-slick-import",
     description="A utility that imports data into the slick test manager",
@@ -16,7 +20,10 @@ setup(
     packages=find_packages(),
     package_data={'': ['*.txt', '*.rst', '*.html']},
     include_package_data=True,
-    install_requires=['slickqa>=2.0.81',],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': ['slick-import = slickimport.main:main',],
+    }
     author="Slick Developers",
     url="http://github.com/slickqa/slick-import"
 )
